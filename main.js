21ac8c45e768bd7f731bde7d6daee528941328b7
Colin Stell <ColinStellDev96@users.noreply.github.com>
2017-08-04 15:07:48 -0600

    final piece and comments

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
 
     /*-=-=-= Total Time Spent on Page =-=-=- */
+    /* Will Fire on Unload */
 
         var startTime = Date.now();
         var endTime;
         $(window).on('beforeunload', function(){
             endTime = Date.now();
-            console.log(endTime - startTime);
+            console.log("Time on Page: " + (endTime - startTime));
         });
 
     /*-=-=-= What Percentage of Page Was Viewed =-=-=-*/
+    /* Will Fire on Unload */
 
         var maxScroll = 0;
         var windowH = $(window).height();
@@ -21,10 +23,11 @@ $(document).ready(function(){
             maxScroll = percentScroll;
         }
         $(window).on('beforeunload', function() {
-            console.log(maxScroll + '% scrolled');
+            // console.log(maxScroll + '% scrolled');
         });
 
     /*-=-=-= Time Spent Hovering Mouse Over Each Element on Page =-=-=-*/
+    /* Will appear in console.log as you hover over element */
 
         var startHover;
         var endHover;
@@ -33,29 +36,39 @@ $(document).ready(function(){
         }, function (event) {
             exitHover = Date.now();
             hoverTime = exitHover - startHover;
-            console.log(hoverTime);
+            // console.log("hover time: " + hoverTime);
         });
 
 
     /*-=-=-= How Many Vowels Types Into Text Area =-=-=-*/
+    /* Will appear in console.log as after you click out of text area */
 
+    var textInput = [];
 
-        $('#textArea').on('keypress', function (){
-            var $textInput = $('#textArea').val().length;
-            var vowels = [65, 69, 73, 79, 85, 97, 101, 105, 111, 117];
-            var vowelCount = 0;
-                for(var i = 0; i < $textInput.length; i++){
-                    if ($textInput.indexOf($textInput[i] !== -1)){
-                        vowelCount.push($textInput[i]);
-                        console.log(vowelCount);
-                    }
-                }
-        });
+    var findVowels = function (array) {
+        var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+        var vowelCount = 0;
+        for(var i = 0; i < array.length; i++) {
+            if (vowels.indexOf(array[i]) !== -1) {
+                vowelCount += 1;
+            }
+        } return vowelCount;
+    };
+
+    $('#textArea').on('blur', function (){
+            console.log(findVowels(textInput) + " vowels");
+    });
+
+    $('#textArea').keypress(function (){
+        var text = $('#textArea').val();
+        textInput.push(text[text.length - 1]);
+    });
 
     /*-=-=-= Left By Link =-=-=-*/
+    /* will appear in preserve console log as you leave page via link */
 
         $('a').click(function(event){
-            console.log($(this).attr('href'));
+            // console.log("User Left Via:" + ($(this).attr('href')));
         });
 
 });
